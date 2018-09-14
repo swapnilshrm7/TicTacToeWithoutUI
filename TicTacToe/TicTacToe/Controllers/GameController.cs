1a2d62edd67b4bf4fdcb4eb5ce58cbb1d0578391
swapnilshrm7 <38393796+swapnilshrm7@users.noreply.github.com>
2018-09-14 18:43:25 +0530

    Update GameController.cs

--- TicTacToe/TicTacToe/Controllers/GameController.cs
+++ TicTacToe/TicTacToe/Controllers/GameController.cs
@@ -11,5 +11,44 @@ namespace TicTacToe.Controllers
     [ApiController]
     public class GameController : ControllerBase
     {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+        DBAccess db = new DBAccess();
+        // GET: api/Game
+        [HttpGet]
+        public string Get()
+        {
+            string status=db.GetStatus();
+            return status;
+        }
+
+        // GET: api/Game/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Game
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Game/5
+        [HttpPut]
+        public void Put([FromBody]Token AccessToken)
+        {
+            db.StartGameWith(AccessToken);
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
     }
 }
